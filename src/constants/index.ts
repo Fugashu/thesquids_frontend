d2f67e61d697636776e7bf92e3be80e6407c00cb
fugashu <friesslich.jonas@gmail.com>
2022-06-12 12:00:26 +0200

    discord bot

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,11 +1,15 @@
-export const PRODUCTION = false;
+export const PRODUCTION = true;
 
 export const desktopBreakPoint = 1224;
 export const backendEndpoint = PRODUCTION
   ? "https://minting.dns.army"
   : "https://minting.dns.army/test";
-export const clientSecret = "-3SU_sNnbtvMOgWnhSPcJxRV3QOa0JnZ";
-export const clientId = "970248803808587797";
+export const clientSecret = PRODUCTION
+  ? "-3SU_sNnbtvMOgWnhSPcJxRV3QOa0JnZ"
+  : "JT-iFy9whjkxdlfBc35AU28UPb7hN5ON";
+export const clientId = PRODUCTION
+  ? "970248803808587797"
+  : "985483147615563836";
 export const redirectUrl = PRODUCTION
   ? "https://thesquids.io/#/app2"
   : "http://localhost:3000/#/app2";
