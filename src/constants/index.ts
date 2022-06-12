b45c7e7303a9fc6ae5ec498990c8bf0d323dc23d
fugashu <friesslich.jonas@gmail.com>
2022-06-12 11:04:39 +0200

    localhost https-> http

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -8,5 +8,5 @@ export const clientSecret = "-3SU_sNnbtvMOgWnhSPcJxRV3QOa0JnZ";
 export const clientId = "970248803808587797";
 export const redirectUrl = PRODUCTION
   ? "https://thesquids.io/#/app2"
-  : "https://localhost:3000/#/app2";
+  : "http://localhost:3000/#/app2";
 export const tutorialVideoUrl = "https://player.vimeo.com/video/712188045";
